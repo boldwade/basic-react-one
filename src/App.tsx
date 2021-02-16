5c35a422e469b4a3b5bdbf2a0a94594ccfe5546d
Wade Walker <wwalker@pksti.com>
2021-02-16 09:25:14 -0600

    Initialize project using Create React App

---
+++ src/App.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import './App.scss';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import Login from "./Login";
+
+export default function App() {
+    return (
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link to="/login">Login</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+                <Switch>
+                    <Route path="/about">
+                        <About />
+                    </Route>
+                    <Route path="/login">
+                        <Login />
+                    </Route>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
+}
+
+function Home() {
+    return <h2>Home</h2>;
+}
+
+function About() {
+    return <h2>About</h2>;
+}
+
+function Users() {
+    return <h2>Users</h2>;
+}
+
